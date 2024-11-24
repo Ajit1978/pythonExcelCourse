b4a3abcec8334771a3cd649ae4ce71e044a7f8ce
zahid shaikh <munjajit@gmail.com>
2024-11-24 13:30:12 +0530

    day 5

---
+++ readWritePandas/updatesafedata.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from openpyxl import load_workbook
+
+
+file_path = "test.xlsx"
+
+
+wb=load_workbook(file_path)
+sheet_name="Sheet1"
+ws=wb[sheet_name]
+
+ws["A1"]="updated 555"
+wb.save(file_path)
+
+
+print ("data updated successfully")
