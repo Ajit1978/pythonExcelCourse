8089a8f7e27978515ee93b595228de21fcba3f7e
Shravani3031 <zahidbhaimbbs@gmaIL.COM>
2024-12-29 13:36:02 +0530

    Day8

--- dataManipulation/dataManipulation.py
+++ dataManipulation/dataManipulation.py
@@ -4,7 +4,13 @@ import pandas as pyExcel
 # df=pd.read_excel('BloodData.xlsx','Sheet2')
 # print(df)
 
-df=pd.read_excel('Crime Prediction.xlsx','Chicago Crime Prediction')
-print(df)
-
-sd=pd.describe_excel('Crime Prediction.xlsx','Chicago Crime Prediction')
+data=pd.read_excel('Crime Prediction.xlsx','Chicago Crime Prediction')
+# print(data)
+# print(data.head())
+data.head().to_excel("Write Head.xlsx",sheet_name="Sheet1")
+# print('--------------------------------------------------')
+# print(data.info())
+data.info().to_excel("Write Info.xlsx",sheet_name="Sheet1")
+# print('--------------------------------------------------')
+# print(data.describe())
+data.describe().to_excel("Write Describe.xlsx",sheet_name="Sheet1")
